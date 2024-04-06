9b31693f629b81c8b9ff3cbcc63cfb9115b677a7
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-06 22:50:37 +0000

    Loop de repetição For

---
+++ src/main/java/Linguagem/For.java
@@ -0,0 +1,13 @@
+package Linguagem;
+
+public class For{
+    public static void main(String[] args) {
+      
+      int numero = 8;
+
+      for(int i = 0; i < numero; i++){
+        System.out.println(i);
+      }
+      
+        }
+    }
