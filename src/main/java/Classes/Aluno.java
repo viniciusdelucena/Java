ec8aa1e7e2851831fafa82a522130263836e31cf
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-06 01:51:07 +0000

    Criação de classe com construtor e modificadores de acesso

---
+++ src/main/java/Classes/Aluno.java
@@ -0,0 +1,27 @@
+package Classes;
+
+public class Aluno{
+  private String nome;
+  private int matricula;
+
+  Aluno(String nome, int matricula){
+    this.nome = nome;
+    this.matricula = matricula;
+  }  
+  
+  public String getNome(){
+    return nome;
+  } 
+  
+  public void setNome(String nome){
+    this.nome = nome;
+  }  
+  
+  public int getMatricula(){
+    return matricula;
+  }  
+  
+  public void setMatricula(int matricula){
+    this.matricula = matricula;
+  }
+}
