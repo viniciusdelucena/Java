f3696e22d3b08381d638b5f245964dae2c1c92ce
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-06 02:32:08 +0000

    Implementação de atributo static, criação de metodo de impressão e classe para impressão.

--- src/main/java/Classes/Aluno.java
+++ src/main/java/Classes/Aluno.java
@@ -3,11 +3,18 @@ package Classes;
 public class Aluno{
   private String nome;
   private int matricula;
+  private static int idadeMaxima = 130;
 
   Aluno(String nome, int matricula){
     this.nome = nome;
     this.matricula = matricula;
   }  
+
+  public void imprimeInformacoes(){
+    System.out.println("Nome do aluno: " + this.nome);
+    System.out.println("Matricula do aluno: " + this.matricula);
+    System.out.println("Idade máxima do aluno: " + Aluno.idadeMaxima);
+  }
   
   public String getNome(){
     return nome;
@@ -23,5 +30,13 @@ public class Aluno{
   
   public void setMatricula(int matricula){
     this.matricula = matricula;
+  }
+
+  public int getIdadeMaxima(){
+    return idadeMaxima;
+  }
+  
+  public void setIdadeMaxima(int idadeMaxima){
+    Aluno.idadeMaxima = idadeMaxima;
   }
 }
---
+++ src/main/java/Classes/TestAluno.java
@@ -0,0 +1,11 @@
+package Classes;
+
+public class TestAluno{
+
+  public static void main(String [] args){
+    Aluno a1 = new Aluno("Vinicius", 202418);
+
+    a1.imprimeInformacoes();
+  }
+
+}
