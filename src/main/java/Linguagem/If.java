a3e492e2e39600e41feca8e1950ddd08133108f2
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-06 23:06:42 +0000

    Implementação do if

---
+++ src/main/java/Linguagem/If.java
@@ -0,0 +1,11 @@
+package Linguagem;
+
+public class If{
+  public static void main(String [] args){
+    int idade = 0;
+    
+    if(idade < 18){
+      System.out.println("Você é menor de idade.");
+    }
+  }
+}
