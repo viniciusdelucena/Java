d9abc0c7bca8cbd37282e224dbaed02880d7dc87
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-07 00:07:36 +0000

    Implementação do while.

---
+++ src/main/java/Linguagem/While.java
@@ -0,0 +1,16 @@
+package Linguagem;
+
+public class While{
+
+  public static void main(String [] args){
+    int idade = 0;
+
+    while (idade < 5){
+      idade++;
+    }
+
+    if(idade > 1){
+      System.out.println(idade);
+    }
+  }  
+}
