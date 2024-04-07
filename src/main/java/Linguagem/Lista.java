d0759bda053623d394cf72325281af4151ac201e
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-07 14:25:48 +0000

    Implementação ArrayList.

---
+++ src/main/java/Linguagem/Lista.java
@@ -0,0 +1,29 @@
+package Linguagem;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Lista{
+
+  public static void main(String[] args){
+    
+    List<String> nomes = new ArrayList<>();
+    
+    nomes.add("Vinicius");
+    nomes.add("Rafael");
+    nomes.add("Orlando");
+    nomes.remove(0);
+
+    for(int i = 0; i < nomes.size(); i++){
+      System.out.println("Nome: " + nomes.get(i));
+    }
+  
+
+  List<Integer> numeros = new ArrayList<>(List.of(0, 1, 2, 3, 4, 5));
+
+  for(int numero : numeros){
+    System.out.println("numero: " + numero);
+  }
+  
+}
+}
