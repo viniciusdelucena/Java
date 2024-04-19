5bb66d1ee7afcdd5009048fcacdb7a67c1497d9d
Vinicius de Lucena <101951162+viniciusdelucena@users.noreply.github.com>
2024-04-19 00:24:02 +0000

    Método retirarInicio, inserirFim e retirarFim adicionados na LSEC

--- src/main/java/EstruturaDeDados/ListaSimplesEncadeadaCircular/ListaSimplesCircular.java
+++ src/main/java/EstruturaDeDados/ListaSimplesEncadeadaCircular/ListaSimplesCircular.java
@@ -24,7 +24,52 @@ public class ListaSimplesCircular{
   public String retirarInicio(){
     if(inicio == null){
       return null;
+    } 
+    String info = inicio.info;
+    inicio = inicio.proximo;
+    No apontaPraAtual = inicio;
+    while(apontaPraAtual.proximo != null){
+      apontaPraAtual = apontaPraAtual.proximo;
     }
-    return null;
+    apontaPraAtual.proximo = inicio;
+    tamanho--;
+    
+    return "Retirado" + info;
   }
+
+  public void inserirFim(String info){
+    No no = new No();
+    no.info = info;
+    if(inicio == null){
+      inicio = no;
+    } else{
+      No apontaPraAtual = inicio;
+      while(apontaPraAtual.proximo != null){
+        apontaPraAtual = apontaPraAtual.proximo;
+      }
+      apontaPraAtual.proximo = no;
+    }
+    no.proximo = inicio;
+    tamanho++;
+  }
+
+  public String retirarFim(){
+    if(inicio == null){
+      return null;
+    }
+    No apontaPraAtual = inicio;
+    while(apontaPraAtual.proximo != null){
+      No aux = apontaPraAtual;
+      apontaPraAtual = apontaPraAtual.proximo;
+      if(apontaPraAtual.proximo == null){
+        aux.proximo = inicio;
+        tamanho--;
+        return apontaPraAtual.info;
+      }
+    }
+    inicio = null;
+    tamanho--;
+    return apontaPraAtual.info;
+  }
+  
 }
